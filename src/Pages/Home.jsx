c5a466888b82727a20159d9e1f3cdae0336769e0
Mansjoer <ramabljredit@gmail.com>
2023-10-05 04:22:56 +0700

    Wip: Search query params from urls that sended from github auth

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export const Home = () => {
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const codeParams = urlParams.get("code");
+    console.log(codeParams);
+  }, []);
+
   return (
     <>
       <h1 className="font-bold text-2xl">Ini Home</h1>

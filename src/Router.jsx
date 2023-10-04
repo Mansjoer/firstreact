cc6eba880b30c0c3dbc71264ff1fd3555bcb281b
Mansjoer <ramabljredit@gmail.com>
2023-10-05 04:14:21 +0700

    Feat: Add /login page, Add axios packages

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./Pages/Home";
+import Login from "./Pages/Auth/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ]);
 
